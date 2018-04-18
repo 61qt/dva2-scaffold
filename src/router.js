4c78bb117e5b19e591e08de53d41f9c678484951
神奇辉 <shenqihui0920@gmail.com>
2018-04-18 14:03:59 +0800

    命名格式修改。

--- src/router.js
+++ src/router.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
-import IndexPage from './routes/IndexPage';
+import index from './routes/index';
 
+const AdminRoute = [];
+const WelcomeRoute = [];
+
+if (__DEV__ && __PROD__) {
+  // 强制 false ，不会输出进来的
+  window.console.log(AdminRoute, WelcomeRoute);
+}
+
+// function RouterConfig({ history, app, location }) {
 function RouterConfig({ history }) {
+  // eslint-disable-next-line no-underscore-dangle
+  // const dispatch = app._store.dispatch;
   return (
     <Router history={history}>
+      {
+        /*
+          <Route path="/welcome" component={Welcome} onEnter={WelcomeOnEnter({ dispatch, location, history })}>
+            { WelcomeRoute }
+          </Route>
+          <Route path="/admin" component={Admin} onEnter={AdminOnEnter({ dispatch, location, history })}>
+            { AdminRoute }
+          </Route>
+        */
+      }
       <Switch>
-        <Route path="/" exact component={IndexPage} />
+        <Route path="/" exact component={index} />
       </Switch>
     </Router>
   );
