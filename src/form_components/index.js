a3842f7fb15912299667e78d311a4764bb486f12
神奇辉 <shenqihui0920@gmail.com>
2018-04-18 20:35:58 +0800

    统一导出 src/form_components

---
+++ src/form_components/index.js
@@ -0,0 +1,13 @@
+import DateRange from './date_range';
+import ForeignSelect from './foreign_select';
+import ImageUploader from './image_uploader';
+import TimeRange from './time_range';
+import WeekRange from './week_range';
+
+export default {
+  DateRange,
+  ForeignSelect,
+  ImageUploader,
+  TimeRange,
+  WeekRange,
+};
