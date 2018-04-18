7533e7266432b9f442ecafd335598a632fc3e89f
神奇辉 <shenqihui0920@gmail.com>
2018-04-18 19:30:06 +0800

    完善 constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,17 +1,17 @@
 import DICT from './dict';
 
-export { DICT };
-
-export default {
+const CONSTANTS = {
   DICT,
   PAGE_SIZE: 30,
   // 直接获取全部数据，会使用到。
   PAGE_SIZE_MAX: 9999,
   API_URL_BASE: DEFINE_API_URL_BASE,
+  QINIU_IMG_CDN_URL: 'https://static-cdn.qiniu.cn/',
   NOTIFICATION_DURATION: 10,
-  TOKEN: 'token',
-  TOKEN_OFFSET: 'token_offset',
   QINIU_TOKEN: 'qiniu_token',
   AREA_KEY: 'AREA_CACHE_V1',
-  BASE64_REGEXP: /^data:image\/[^;]+?;base64,/,
 };
+
+export { DICT };
+
+export default CONSTANTS;
