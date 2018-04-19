4c82a5df93c4c239b7d50668d6818a51d50b5557
神奇辉 <shenqihui0920@gmail.com>
2018-04-19 15:19:51 +0800

    app 路径，完成。

--- src/router.js
+++ src/router.js
@@ -3,6 +3,8 @@ import { Router, Switch, Route, Redirect } from 'dva/router';
 
 // 例子模块，exact 精确匹配
 import example from './routes/example';
+// 已经授权的模块，非精确匹配
+import App from './routes/app';
 // 获取授权的模块，非精确匹配
 import Welcome from './routes/welcome';
 
@@ -11,7 +13,9 @@ function RouterConfig({ history }) {
     <Router history={history}>
       <Switch>
         <Route path="/example" exact component={example} />
+        <Route path="/app" component={App} />
         <Route path="/welcome" component={Welcome} />
+        <Redirect to="/app" />
       </Switch>
     </Router>
   );
