518596a62f5c2d5db78f375e204dc73e7e7eff16
神奇辉 <shenqihui0920@gmail.com>
2018-04-19 14:55:26 +0800

    修复 Object.assign config.header 为复数

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,17 +2,13 @@ import axios from 'axios';
 import User from '../utils/user';
 
 const http = axios.create({
-  baseURL: 'https://api.example.com/',
-  timeout: 10000,
-  headers: {
-    'X-Custom-Header': 'foobar',
-  },
+  baseURL: 'https://zhsng-sng-api.61qt.cn/',
 });
 
 // Add a request interceptor
 http.interceptors.request.use((config) => {
   if (User.token) {
-    Object.assign(config.header, {
+    Object.assign(config.headers, {
       Authorization: `Bearer ${User.token}`,
     });
   }
