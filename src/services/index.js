943ec7fb5d9ff4d0d049cfd9807a69e76ff17a74
神奇辉 <shenqihui0920@gmail.com>
2018-04-19 17:08:10 +0800

    增加 sys_message services

--- src/services/index.js
+++ src/services/index.js
@@ -1,8 +1,11 @@
 import common from './common';
 import student from './student';
+// eslint-disable-next-line camelcase
+import sys_message from './sys_message';
 
 export default {
   common,
   student,
+  sys_message,
 };
 
---
+++ src/services/sys_message.js
@@ -0,0 +1,9 @@
+import Factory from '../services/_factory';
+
+const namespace = 'common/sys_message';
+
+const Service = Factory({
+  namespace,
+});
+
+export default Service;
