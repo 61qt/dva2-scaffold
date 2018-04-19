7f7bfbc0a3ea9674bf19359464288fd1c3332675
神奇辉 <shenqihui0920@gmail.com>
2018-04-19 17:08:57 +0800

    src/router.js 增加 LocaleProvider

--- src/router.js
+++ src/router.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import _ from 'lodash';
 import { Router, Switch, Route, Redirect } from 'dva/router';
+import { locales, LocaleProvider } from 'antd';
 
 // 例子模块，exact 精确匹配
 import example from './routes/example';
@@ -8,8 +10,10 @@ import App from './routes/app';
 // 获取授权的模块，非精确匹配
 import Welcome from './routes/welcome';
 
+const zhCN = _.get(locales, 'zh_CN');
+
 function RouterConfig({ history }) {
-  return (
+  return (<LocaleProvider locale={zhCN}>
     <Router history={history}>
       <Switch>
         <Route path="/example" exact component={example} />
@@ -18,7 +22,7 @@ function RouterConfig({ history }) {
         <Redirect to="/app" />
       </Switch>
     </Router>
-  );
+  </LocaleProvider>);
 }
 
 export default RouterConfig;
