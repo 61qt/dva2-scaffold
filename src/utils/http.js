a4e2d38cdac9d3214102416896c6678fe8925ec7
神奇辉 <shenqihui0920@gmail.com>
2018-04-19 14:26:30 +0800

    src/utils/http.js 增加 action 字段。

--- src/utils/http.js
+++ src/utils/http.js
@@ -3,16 +3,18 @@ import jQuery from 'jquery';
 const http = jQuery.ajax;
 http.get = jQuery.get;
 http.post = jQuery.post;
-http.put = (url, values) => {
+http.put = (url, values, options = {}) => {
   return jQuery.ajax(url, {
     method: 'PUT',
     data: values,
+    ...options,
   });
 };
-http.delete = (url, values) => {
+http.delete = (url, values, options = {}) => {
   return jQuery.ajax(url, {
     method: 'DELETE',
     data: values,
+    ...options,
   });
 };
 
