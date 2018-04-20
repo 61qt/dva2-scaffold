e17cecd5818820eacac010e4119f87a434341f81
神奇辉 <shenqihui0920@gmail.com>
2018-04-20 19:29:26 +0800

    add specialty

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,6 @@
 import common from './common';
 import post from './post';
+import specialty from './specialty';
 import student from './student';
 // eslint-disable-next-line camelcase
 import sys_message from './sys_message';
@@ -8,6 +9,7 @@ import teacher from './teacher';
 export default {
   common,
   post,
+  specialty,
   student,
   sys_message,
   teacher,
---
+++ src/services/specialty.js
@@ -0,0 +1,9 @@
+import Factory from '../services/_factory';
+
+const namespace = 'specialty';
+
+const Service = Factory({
+  namespace,
+});
+
+export default Service;
