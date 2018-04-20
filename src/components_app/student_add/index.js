527c6d8f4e4e402b3bed57c950d9bbee4850edd7
神奇辉 <shenqihui0920@gmail.com>
2018-04-20 15:59:15 +0800

    优化 student 表单

--- src/components_app/student_add/index.js
+++ src/components_app/student_add/index.js
@@ -1,11 +1,38 @@
+import moment from 'moment';
 import React from 'react';
 import _ from 'lodash';
 import { connect } from 'dva';
-import { Spin, message } from 'antd';
+import { message, Spin, Select, DatePicker, Radio, Button, Form, Col, Row, Icon } from 'antd';
+import Well from '../../components_atom/well';
 import styles from './index.less';
-import AddForm from './form';
 import Services from '../../services';
+import FormComponents from '../../components_form';
+import Filters from '../../filters';
 import formErrorMessageShow from '../../utils/form_error_message_show';
+import buildColumnFormItem from '../../utils/build_column_form_item';
+import formatFormValue from '../../utils/format_form_value';
+
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 6 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 14 },
+  },
+};
+
+const formTailItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 6 },
+  },
+  wrapperCol: {
+    xs: { span: 24, offset: 0 },
+    sm: { span: 14, offset: 6 },
+  },
+};
 
 class Component extends React.Component {
   constructor(props) {
@@ -18,9 +45,214 @@ class Component extends React.Component {
       method: false === paramsId ? 'create' : 'update',
     };
     this.state = {
+      expand: !!paramsId,
+      col: 24,
+      formValidate: {},
+      submitting: false,
       loading: true,
       dataSource: false,
     };
+    this.columns = [
+      {
+        title: '学生头像',
+        dataIndex: 'avatar',
+        render: (defaultValue) => {
+          const newOptions = {
+            initValue: defaultValue,
+            options: {
+              width: 150,
+              height: 150,
+            },
+          };
+
+          return (
+            <FormComponents.ImageUploader {...newOptions} />
+          );
+        },
+        initialValue: (text, dataSource) => {
+          return dataSource.avatar || '';
+        },
+        shouldInitialValue: true,
+        rules: [{
+          required: true, message: '必须上传学生头像',
+        }],
+      },
+      {
+        title: '学生姓名',
+        dataIndex: 'name',
+        rules: [{
+          required: true, message: '学生姓名必填',
+        }],
+      },
+      {
+        title: '出生日期',
+        dataIndex: 'birthday',
+        render: () => {
+          return <DatePicker />;
+        },
+        initialValue: (text = moment().add(-10, 'year').unix()) => {
+          return moment.unix(text);
+        },
+        rules: [{
+          required: true, message: '必须选择出生日期',
+        }],
+      },
+      {
+        title: '性别',
+        dataIndex: 'gender',
+        render: () => {
+          return (<Radio.Group options={Filters.dict(['student', 'gender'])} />);
+        },
+        rules: [{
+          required: true, message: '必须选择性别',
+        }],
+      },
+      {
+        title: '主要联系人姓名',
+        dataIndex: 'primary_name',
+        rules: [{
+          required: true, message: '必须填写主要联系人姓名',
+        }],
+      },
+
+      {
+        title: '主要联系人电话',
+        dataIndex: 'primary_phone',
+        inputNumberOptions: {
+          className: 'ant-input-number-row',
+          min: 13000000000,
+          max: 19999999999,
+        },
+        zeroEmptyFlag: true,
+        rules: [{
+          required: true, message: '必须填写主要联系人电话',
+        }, {
+          validator: (rule, value, callback) => {
+            if (value && (13000000000 > value || 19999999999 < value)) {
+              callback('手机号格式不正确');
+            }
+            callback();
+          },
+        }],
+      },
+      {
+        title: '身份证号',
+        dataIndex: 'id_number',
+      },
+      {
+        title: '所在学校',
+        dataIndex: 'school',
+        render: () => {
+          const options = {
+            valueName: 'name', // 用于 options 的 value dataIndex ， 默认不传输为 id 。
+            textName: 'name', // 用于 options 的 label dataIndex ， 默认不传输为 name 。
+          };
+          return (<FormComponents.ForeignSelect placeholder="所在学校" url="school" search={{ format: 'filter', name: 'name', method: 'like' }} allowClear options={options} mode="combobox" />);
+        },
+      },
+      {
+        title: '学生手机',
+        dataIndex: 'phone',
+        inputNumberOptions: {
+          // className 带有 ant-input-number-row 代表长度为 100% 。
+          className: 'ant-input-number-row',
+          min: 13000000000,
+          max: 19999999999,
+        },
+        zeroEmptyFlag: true,
+      },
+      {
+        title: '次要联系人姓名',
+        dataIndex: 'secondary_name',
+        rules: [{
+          validator: (rule, value, callback) => {
+            props.form.validateFields(['secondary_phone'], { force: true });
+            callback();
+          },
+        }],
+      },
+      {
+        title: '次要联系人电话',
+        dataIndex: 'secondary_phone',
+        inputNumberOptions: {
+          className: 'ant-input-number-row',
+          min: 13000000000,
+          max: 19999999999,
+        },
+        zeroEmptyFlag: true,
+        rules: [{
+          validator: (rule, value, callback) => {
+            if (value && (13000000000 > value || 19999999999 < value)) {
+              callback('手机号格式不正确');
+            }
+            const secondaryName = props.form.getFieldValue('secondary_name');
+            if ((!value && secondaryName) || (value && !secondaryName)) {
+              callback('次要联系人姓名和次要联系人电话需要同时填写');
+            }
+            else {
+              callback();
+            }
+          },
+        }],
+      },
+      {
+        title: '家庭住址',
+        dataIndex: 'home_address',
+      },
+      {
+        title: '邮箱地址',
+        dataIndex: 'email',
+      },
+      {
+        title: '来源',
+        dataIndex: 'source',
+        render: () => {
+          return (<Radio.Group options={Filters.dict(['student', 'source'])} />);
+        },
+      },
+      {
+        title: '身高(cm)',
+        dataIndex: 'height',
+        inputNumberOptions: {
+          min: 1,
+          max: 300,
+        },
+        zeroEmptyFlag: true,
+      },
+      {
+        title: '体重(kg)',
+        dataIndex: 'weight',
+        inputNumberOptions: {
+          min: 1,
+          max: 999,
+        },
+        zeroEmptyFlag: true,
+      },
+
+      {
+        title: '鞋码',
+        dataIndex: 'shoes',
+        inputNumberOptions: {
+          min: 1,
+          max: 999,
+        },
+        zeroEmptyFlag: true,
+      },
+
+      {
+        title: '民族',
+        dataIndex: 'nation',
+        render: () => {
+          return (<Select allowClear>
+            {
+              Filters.dict(['nation']).map((elem) => {
+                return (<Select.Option key={elem.value} value={elem.value}>{elem.label}</Select.Option>);
+              })
+            }
+          </Select>);
+        },
+      },
+    ];
   }
 
   componentDidMount = () => {
@@ -66,7 +298,85 @@ class Component extends React.Component {
     }
   }
 
-  handleSubmit = ({ values, errorCallback, successCallback }) => {
+  resetFormValidate = () => {
+    const formValidate = {};
+    this.columns.forEach((elem) => {
+      const dataIndex = elem.key || elem.dataIndex;
+      formValidate[dataIndex] = {};
+    });
+
+    this.setState({
+      formValidate,
+    });
+  }
+
+  // 提交表单正确时候的处理。
+  successCallback = () => {
+    this.setState({
+      submitting: false,
+    });
+  }
+
+  // 提交表单错误时候的处理。
+  errorCallback = (value) => {
+    const formValidate = this.state.formValidate;
+    for (const [k] of Object.entries(formValidate)) {
+      formValidate[k] = {};
+    }
+
+    for (const [k, v] of Object.entries(value)) {
+      formValidate[k] = {
+        validateStatus: 'error',
+        help: _.get(v, '[0]') || v,
+      };
+    }
+    this.setState({
+      formValidate,
+    });
+    this.setState({
+      submitting: false,
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.submitting) {
+      message.info('正在提交');
+      return;
+    }
+    this.resetFormValidate();
+    const form = this.props.form;
+    form.validateFieldsAndScroll((err, values) => {
+      if (err) {
+        formErrorMessageShow(err);
+      }
+      if (!err) {
+        const formattedValues = {
+          ...values,
+        };
+        if (values.birthday) {
+          formattedValues.birthday = values.birthday
+            .clone()
+            .hour(0)
+            .minute(0)
+            .second(0)
+            .unix();
+        }
+        for (const [key, value] of Object.entries(formattedValues)) {
+          formattedValues[key] = formatFormValue(value);
+        }
+        this.state.submitting = true;
+        this.setState({
+          submitting: true,
+        });
+        this.handleSubmitRun({
+          values: formattedValues,
+        });
+      }
+    });
+  }
+
+  handleSubmitRun = ({ values }) => {
     const formData = {
       ...values,
     };
@@ -79,9 +389,7 @@ class Component extends React.Component {
     }
     promise.then(() => {
       message.success(`${this.editInfo.text}学生成功`);
-      if ('function' === typeof successCallback) {
-        successCallback();
-      }
+      this.successCallback();
       const { location, history } = this.props;
       const dt = _.get(location, 'query.dt');
       if (dt) {
@@ -90,20 +398,75 @@ class Component extends React.Component {
       else {
         history.push('/app/student');
       }
-    })
-      .catch((rej) => {
-        formErrorMessageShow(rej);
-        if ('function' === typeof errorCallback) {
-          errorCallback(rej.data);
-        }
-      });
+    }).catch((rej) => {
+      formErrorMessageShow(rej);
+      this.errorCallback(rej.data);
+    });
+  }
+
+  handleReset = () => {
+    this.props.form.resetFields();
+  }
+
+  toggle = () => {
+    const { expand } = this.state;
+    this.setState({ expand: !expand });
+  }
+
+  renderForm = () => {
+    const children = buildColumnFormItem({
+      ...this.props,
+      ...this.state,
+      columns: this.columns,
+      shouldInitialValue: this.editInfo.paramsId,
+      defaultValueSet: this.state.dataSource,
+      formItemLayout,
+      formValidate: this.state.formValidate,
+      col: this.state.col,
+    });
+
+    const expand = this.state.expand;
+    const shownCount = 6;
+    return (
+      <Spin spinning={this.state.submitting}>
+        <Form
+          className={`app-edit-form ${expand ? '' : 'is-close'}`}
+          onSubmit={this.handleSubmit}
+        >
+          <Well title={`${this.editInfo.text}学生`}>
+            <Row gutter={40}>
+              {children.slice(0, shownCount)}
+            </Row>
+            <Row className={!expand ? 'ant-hide' : ''} gutter={40}>
+              {children.slice(shownCount)}
+            </Row>
+          </Well>
+          <Well holderplace>
+            <Row gutter={40}>
+              <Col span={this.state.col}>
+                <Form.Item {...formTailItemLayout}>
+                  <Button size="default" type="primary" htmlType="submit" disabled={this.state.submitting} loading={this.state.submitting}>保存</Button>
+                  <Button size="default" style={{ marginLeft: 8 }} onClick={this.handleReset}>
+                    重置
+                  </Button>
+
+                  <a style={{ marginLeft: 8, fontSize: 12 }} onClick={this.toggle}>
+                    { expand ? '收起' : '展开' } <Icon type={expand ? 'up' : 'down'} />
+                  </a>
+                </Form.Item>
+              </Col>
+            </Row>
+          </Well>
+        </Form>
+      </Spin>
+    );
   }
 
   render() {
     return (
       <div className={styles.normal}>
         <Spin spinning={this.state.loading}>
-          { this.state.dataSource && !this.state.loading ? <AddForm {...this.props} loading={this.state.loading} editInfo={this.editInfo} params={this.props.params} handleSubmit={this.handleSubmit} defaultValue={this.state.dataSource} /> : <div>正在加载</div>}
+          { this.state.dataSource && !this.state.loading ? this.renderForm() : <div>正在加载</div>}
         </Spin>
       </div>
     );
@@ -115,4 +478,4 @@ function mapStateToProps() {
   };
 }
 
-export default connect(mapStateToProps)(Component);
+export default connect(mapStateToProps)(Form.create()(Component));
