4f0d32dd145e5b5cff8f304fd02160c17bdb2467
神奇辉 <shenqihui0920@gmail.com>
2018-04-20 11:41:50 +0800

    add post services

--- src/services/index.js
+++ src/services/index.js
@@ -1,10 +1,12 @@
 import common from './common';
+import post from './post';
 import student from './student';
 // eslint-disable-next-line camelcase
 import sys_message from './sys_message';
 
 export default {
   common,
+  post,
   student,
   sys_message,
 };
---
+++ src/services/post.js
@@ -0,0 +1,9 @@
+import Factory from '../services/_factory';
+
+const namespace = 'mobile/post';
+
+const Service = Factory({
+  namespace,
+});
+
+export default Service;
