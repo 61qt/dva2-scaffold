7095ad8115dd0578f3cf285bfa847f233e43ec24
神奇辉 <shenqihui0920@gmail.com>
2018-04-20 15:47:46 +0800

    src/services/post.js add Service.preview

--- src/services/post.js
+++ src/services/post.js
@@ -1,9 +1,17 @@
-import Factory from '../services/_factory';
+import Factory, { http } from '../services/_factory';
 
 const namespace = 'mobile/post';
 
 const Service = Factory({
   namespace,
 });
+
+Service.preview = (values) => {
+  return http.post('/common/preview', {
+    // 0首页，1文章，2简介，3教师列表
+    preview: 1,
+    config: JSON.stringify(values),
+  });
+};
 
 export default Service;
