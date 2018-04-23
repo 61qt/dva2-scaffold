4a6a2a3d76ae4b56aa82948ca7bbcf8cc9edccf0
神奇辉 <shenqihui0920@gmail.com>
2018-04-23 15:53:17 +0800

    add hotjar

---
+++ src/utils/hotjar.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+// <!-- Hotjar Tracking Code for https://*s.61qt.cn -->
+if (__PROD__) {
+  (function(h,o,t,j,a,r){
+      h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+      h._hjSettings={hjid:DEFINE_HOTJAR_KEY,hjsv:6};
+      a=o.getElementsByTagName('head')[0];
+      r=o.createElement('script');r.async=1;
+      r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+      a.appendChild(r);
+      console.log()
+  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+}
+/* eslint-enable */
