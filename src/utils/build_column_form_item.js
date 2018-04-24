d27b8570fe8b1513574c1db02e69af22e03405bc
神奇辉 <shenqihui0920@gmail.com>
2018-04-24 20:45:15 +0800

    完善 src/utils/build_column_form_item.js ，减少变量。

--- src/utils/build_column_form_item.js
+++ src/utils/build_column_form_item.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
-import { InputNumber, Input, Col, Form } from 'antd';
+import { InputNumber, Col, Input, Form } from 'antd';
+// formItemOptions
 
 function buildColumnFormItem({
   columns = [],
@@ -15,55 +16,56 @@ function buildColumnFormItem({
   const children = [];
   columns.forEach((elem) => {
     // 获取 elem 的配置。
-    const { key, className, style, title, dataIndex, render, hidden, initialValue, inputNumberOptions, zeroEmptyFlag, extra, removeRule, hiddenRule, shouldInitialValue: elemShouldInitialValue, formItemLayout: elemFormItemLayout, props = {}, isInputTexteara = false, placeholder = false } = elem;
+
     // 获取初始值，如果有特定的初始值初始化方法，就调用。
-    let defaultValue = _.get(defaultValueSet, dataIndex);
-    if (initialValue) {
-      defaultValue = initialValue(defaultValue, defaultValueSet, { form, text: defaultValue, dataSource: defaultValueSet });
+    let defaultValue = _.get(defaultValueSet, elem.dataIndex);
+    if (elem.initialValue) {
+      defaultValue = elem.initialValue(defaultValue, defaultValueSet, { form, text: defaultValue, dataSource: defaultValueSet });
     }
     // 如果值为零，就是代表填写为空，后端存储的为 int 型造成
-    else if (zeroEmptyFlag && (0 === defaultValue || '0' === defaultValue)) {
+    else if (elem.zeroEmptyFlag && (0 === defaultValue || '0' === defaultValue)) {
       defaultValue = '';
     }
 
     // 判断是否应该移除
     let rowIsRemove = false;
-    if ('boolean' === typeof removeRule) {
-      rowIsRemove = removeRule;
+    if ('boolean' === typeof elem.removeRule) {
+      rowIsRemove = elem.removeRule;
     }
-    else if ('function' === typeof removeRule) {
-      rowIsRemove = removeRule(defaultValue, defaultValueSet, { form, dataSource: defaultValueSet, text: defaultValue });
+    else if ('function' === typeof elem.removeRule) {
+      rowIsRemove = elem.removeRule(defaultValue, defaultValueSet, { form, dataSource: defaultValueSet, text: defaultValue });
     }
+
     if (rowIsRemove) {
       return null;
     }
 
-    let elemTitle = title;
+    let elemTitle = elem.title;
     if ('function' === typeof elemTitle) {
       elemTitle = elemTitle(defaultValue, defaultValueSet, { form, dataSource: defaultValueSet, text: defaultValue });
     }
 
     // 判断是否为隐藏表单列。
     let rowIsHide = false;
-    if ('boolean' === typeof hiddenRule) {
-      rowIsHide = hiddenRule;
+    if ('boolean' === typeof elem.hiddenRule) {
+      rowIsHide = elem.hiddenRule;
     }
-    else if ('function' === typeof hiddenRule) {
-      rowIsHide = hiddenRule(defaultValue, defaultValueSet, { form, dataSource: defaultValueSet, text: defaultValue });
+    else if ('function' === typeof elem.hiddenRule) {
+      rowIsHide = elem.hiddenRule(defaultValue, defaultValueSet, { form, dataSource: defaultValueSet, text: defaultValue });
     }
     // 生成 from 表单展示的内容，默认为 input ，如果配置了 render ，就调用 render(defaultValue); 如果没配置 render 但是配置了 inputNumberOptions ，就输出 InputNumber
     let elemRender = null;
-    if (render) {
-      elemRender = render(defaultValue, defaultValueSet, { form, text: defaultValue, dataSource: defaultValueSet });
+    if (elem.render) {
+      elemRender = elem.render(defaultValue, defaultValueSet, { form, text: defaultValue, dataSource: defaultValueSet });
     }
-    else if (inputNumberOptions) {
-      elemRender = (<InputNumber {...props} {...inputNumberOptions} disabled={hidden} hidden={hidden} placeholder={placeholder || `请输入${elemTitle}`} />);
+    else if (elem.inputNumberOptions) {
+      elemRender = (<InputNumber {...elem.props} {...elem.inputNumberOptions} disabled={elem.hidden} hidden={elem.hidden} placeholder={elem.placeholder || `请输入${elemTitle}`} />);
     }
-    else if (isInputTexteara) {
-      elemRender = (<Input.TextArea size="default" {...props} disabled={hidden} hidden={hidden} placeholder={placeholder || `请输入${elemTitle}`} />);
+    else if (elem.isInputTexteara) {
+      elemRender = (<Input.TextArea size="default" {...elem.props} disabled={elem.hidden} hidden={elem.hidden} placeholder={elem.placeholder || `请输入${elemTitle}`} />);
     }
     else {
-      elemRender = (<Input size="default" {...props} disabled={hidden} hidden={hidden} placeholder={placeholder || `请输入${elemTitle}`} />);
+      elemRender = (<Input size="default" {...elem.props} disabled={elem.hidden} hidden={elem.hidden} placeholder={elem.placeholder || `请输入${elemTitle}`} />);
     }
 
     // getFieldDecorator options
@@ -71,49 +73,56 @@ function buildColumnFormItem({
       initialValue: undefined,
     };
     // 单个原始不允许设置成初始化。
-    if (false === elemShouldInitialValue) {
-      // window.console.log('elemShouldInitialValue false');
+    if (false === elem.shouldInitialValue) {
+      // window.console.log('elem.shouldInitialValue false');
       options.initialValue = undefined;
     }
     // 单个原始允许设置成初始化。或者全部允许设置成初始化。
-    else if (elemShouldInitialValue || shouldInitialValue) {
-      // window.console.log('elemShouldInitialValue || shouldInitialValue true', options.initialValue);
+    else if (elem.shouldInitialValue || shouldInitialValue) {
+      // window.console.log('elem.shouldInitialValue || shouldInitialValue true', options.initialValue);
       options.initialValue = defaultValue;
     }
     if (elem.rules) {
       options.rules = elem.rules;
     }
-    const elemValidate = formValidate[key || dataIndex] || {};
+    const elemValidate = formValidate[elem.key || elem.dataIndex] || {};
 
     const formItemOptions = {
-      className,
-      style,
+      className: elem.className,
+      style: elem.style,
     };
-    if (extra) {
-      formItemOptions.extra = extra;
-      if ('function' === typeof extra) {
-        formItemOptions.extra = extra(defaultValue, defaultValueSet, { form, text: defaultValue, dataSource: defaultValueSet });
+    if (elem.extra) {
+      formItemOptions.extra = elem.extra;
+      if ('function' === typeof elem.extra) {
+        formItemOptions.extra = elem.extra(defaultValue, defaultValueSet, { form, text: defaultValue, dataSource: defaultValueSet });
       }
     }
 
-    const buildElemFormItemLayout = elemFormItemLayout || {
+    const buildElemFormItemLayout = elem.formItemLayout || {
       ...formItemLayout,
     };
-    const formItem = (<Form.Item key={key || dataIndex} {...formItemOptions} {...buildElemFormItemLayout} label={label ? elemTitle : ''} {...elemValidate} className={rowIsHide ? 'ant-hide' : ''}>
-      { form.getFieldDecorator(dataIndex, options)(elemRender) }
+    const formItem = (<Form.Item key={elem.key || elem.dataIndex} {...formItemOptions} {...buildElemFormItemLayout} label={label ? elemTitle : ''} {...elemValidate} className={`${elem.className} ${rowIsHide ? 'ant-hide' : ''}`}>
+      { form.getFieldDecorator(elem.dataIndex, options)(elemRender) }
     </Form.Item>);
-    // 表单结构化。
+
     if (warpCol) {
+      // 表单结构化。
       children.push((
-        <Col {...formItemOptions} md={col} span={col} sm={24} xs={24} key={key || dataIndex} className={rowIsHide ? 'ant-hide' : className}>
+        <Col {...formItemOptions} md={col} span={col} sm={24} xs={24} key={elem.key || elem.dataIndex} className={`${elem.className} ${rowIsHide ? 'ant-hide' : ''}`}>
           { formItem }
         </Col>
       ));
     }
     else {
+      // 返回原来数据，自行组装。
       children.push({
         ...elem,
+        rowIsHide,
+        rowIsRemove,
         render: () => {
+          if (rowIsRemove) {
+            return null;
+          }
           return formItem;
         },
       });
