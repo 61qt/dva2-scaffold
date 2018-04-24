584eac7255cd0a6514bbe6abecd57cf21e3f5d38
神奇辉 <shenqihui0920@gmail.com>
2018-04-24 15:43:09 +0800

    src/routes/example add icon example

--- src/routes/example/index.js
+++ src/routes/example/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import { connect } from 'dva';
+import { Icon } from 'antd';
 import styles from './index.less';
 
 class Component extends React.Component {
+  constructor(props) {
+    super(props);
+    debugAdd('example', this);
+    this.iconArr = [
+      {
+        name: 'search',
+        // url: require('../../sprites/svg/search.svg'),
+        url: 'search',
+      },
+      {
+        name: 'mail',
+        // url: require('../../sprites/svg/mail.svg'),
+        url: 'mail',
+      },
+    ];
+  }
+
   render() {
     return (
       <div className={styles.normal}>
@@ -12,6 +30,13 @@ class Component extends React.Component {
           <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
           <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
         </ul>
+        <div className={styles.iconList}>
+          {
+            this.iconArr.map((icon) => {
+              return (<Icon key={icon.name} type={icon.url} />);
+            })
+          }
+        </div>
       </div>
     );
   }
